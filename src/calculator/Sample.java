2af3d2eae8c583ef36ef43e964cb22e0c002b4e7
admin <admin@10.231.7.7>
2018-10-31 10:58:10 +0900

    while complete

--- src/calculator/Sample.java
+++ src/calculator/Sample.java
@@ -1,17 +1,31 @@
 package calculator;
-
+import java.util.Scanner;
 public class Sample {
-
 	public static void main(String[] args) {
-		int firstValue = 3;
-		int secondValue = 3;
-		int thirdValue = 3;
-		int result = 0;
-		result += firstValue;
-		result /= secondValue;
-		result *= thirdValue;
-		// TODO Auto-generated method stub
-		System.out.println(result);
+		int result;
+		int inputInt;
+		String inputSymbol;
+		Scanner sc = new Scanner(System.in);
+		result = sc.nextInt();
+		while (sc.hasNext()) {
+			String symbol = sc.next();
+			if("quit".equals(symbol)) break;
+			inputInt = sc.nextInt();
+			if("+".equals(symbol)) {
+				result += inputInt; 
+			} else if("-".equals(symbol)) {
+				result -= inputInt;
+			} else if("*".equals(symbol)) {
+				result *= inputInt;
+			} else if("/".equals(symbol)) {
+				result = (int) (result / inputInt);
+			} else {
+				System.out.println("연산기호 오류");
+			}
+		}
+		sc.close();
+		System.out.println("결과 값: " + result);
+
 	}
 
 }
